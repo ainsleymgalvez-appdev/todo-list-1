a70a567cd9bfc8a8c3f7cbf71508a0b28576c052
Ainsley M Galvez <galvezainsley@gmail.com>
2022-11-06 02:47:14 +0000

    100% done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -26,12 +26,12 @@ class TasksController < ApplicationController
 
     if the_task.valid?
 
-      the_task.status = "Next Up"
+      the_task.status = "next_up"
 
       the_task.save
-      redirect_to("/tasks", { :notice => "Task created successfully." })
+      redirect_to("/", { :notice => "Task created successfully." })
     else
-      redirect_to("/tasks", { :alert => the_task.errors.full_messages.to_sentence })
+      redirect_to("/", { :alert => the_task.errors.full_messages.to_sentence })
     end
   end
 
@@ -39,17 +39,12 @@ class TasksController < ApplicationController
     the_id = params.fetch("path_id")
     the_task = Task.where({ :id => the_id }).at(0)
     the_task.user_id = session.fetch(:user_id)
-    the_task.status = "Next Up"
+    the_task.status = params.fetch("query_status")
 
     if the_task.valid?
-      if the_task.status = "Next Up"
-      the_task.status = "In Progress"
-      
-      elsif the_task.status = "In Progress"
-      the_task.status = "Done"
-      end
 
       the_task.save
+
       redirect_to("/", { :notice => "Task updated successfully."} )
     else
       redirect_to("/", { :alert => the_task.errors.full_messages.to_sentence })
